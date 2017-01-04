c50a8dfea90c834e421f953b29ce40a58d54f96c
Hoang Huynh Man <hoanghuynhman@gmail.com>
2017-01-04 09:11:46 +0700

    ref task#9 make pull to refresh

--- app/src/main/java/com/framgia/englishforkids/ui/fragment/VideosFragment.java
+++ app/src/main/java/com/framgia/englishforkids/ui/fragment/VideosFragment.java
@@ -1,7 +1,9 @@
 package com.framgia.englishforkids.ui.fragment;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -11,9 +13,12 @@ import android.view.ViewGroup;
 import com.framgia.englishforkids.R;
 import com.framgia.englishforkids.data.model.VideoDataController;
 import com.framgia.englishforkids.data.model.VideoModel;
+import com.framgia.englishforkids.util.Constant;
+import com.framgia.englishforkids.util.JsoupPaser;
 import com.framgia.englishforkids.util.LoadItemsAdapter;
 import com.framgia.englishforkids.util.ViewMode;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,6 +34,7 @@ public class VideosFragment extends Fragment {
     private ViewMode mMode = ViewMode.GRID;
     private List<VideoModel> mListVideoModel;
     private int mNumberOfColumnsGrid;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
 
     public static VideosFragment newInstance(int category) {
         VideosFragment fragment = new VideosFragment();
@@ -72,6 +78,17 @@ public class VideosFragment extends Fragment {
             mNumberOfColumnsGrid = getResources().getInteger(R.integer.grid_number_of_columns);
             mLayoutManager = new GridLayoutManager(getActivity(), mNumberOfColumnsGrid);
             mAdapter = new LoadItemsAdapter(getActivity(), mListVideoModel, ViewMode.GRID);
+            mSwipeRefreshLayout = (SwipeRefreshLayout) getView().findViewById(R.id.swipe_refresh);
+            mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                @Override
+                public void onRefresh() {
+                    new PaserAsyncTask().execute();
+                }
+            });
+            mSwipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
         } else if (mNumberOfRandomVideos != 0) {
             mListVideoModel = mDataSource.getRandomVideos(mNumberOfRandomVideos);
             mLayoutManager = new GridLayoutManager(getActivity(), 1);
@@ -97,5 +114,42 @@ public class VideosFragment extends Fragment {
                 filteredList.add(videoModel);
         }
         ((LoadItemsAdapter) mAdapter).updateData(filteredList);
+    }
+
+    public class PaserAsyncTask extends AsyncTask<Void, Void, List<VideoModel>> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected List<VideoModel> doInBackground(Void... voids) {
+            List<VideoModel> listSong;
+            List<VideoModel> listStory;
+            try {
+                listSong = JsoupPaser.getInstance().paserSongs();
+                listStory = JsoupPaser.getInstance().paserSories();
+                if (listSong != null && listSong.size() > 0) {
+                    mDataSource.deleteAllRecord();
+                    mDataSource.insertListVideoData(listSong);
+                    mDataSource.insertListVideoData(listStory);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+            return mCategoryId == Constant.SONG_TYPE ? listSong : listStory;
+        }
+
+        @Override
+        protected void onPostExecute(List<VideoModel> listVideoModel) {
+            super.onPostExecute(listVideoModel);
+            if (listVideoModel != null) {
+                mListVideoModel.clear();
+                mListVideoModel.addAll(listVideoModel);
+                mAdapter.notifyDataSetChanged();
+            }
+            mSwipeRefreshLayout.setRefreshing(false);
+        }
     }
 }
